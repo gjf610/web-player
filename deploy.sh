d4336886d69ddc65bfeb1e7e96ed63071c0a350b
jianfeng <gjf610@yeah.net>
2022-01-21 14:47:17 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,5 @@ yarn build &&
 git add . &&
 git commit -m 'deploy' &&
 git branch -M main &&
-git remote add origin git@github.com:gjf610/web-player.git
 git push -f -u origin main
 echo https://gjf610.github.io/web-player/dist/index.html
