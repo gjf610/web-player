729e13999c65c0d8ec1586e4cfd0c5f5a6a64144
jianfeng <gjf610@yeah.net>
2022-01-21 13:42:00 +0800

    feat: added volume

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const m = {
   audio: new Audio(),
   duration: 0,
   clock: null,
+  cacheVolume: 0,
   musicList: [
     {
       "src": "http://jirengu_1.gitee.io/music/music/ifyou.mp3",
@@ -46,6 +47,11 @@ const $author = $('.player .texts p')
 const $time = $('.player .time')
 const $bar = $('.player .bar')
 const $progress = $('.player .progress')
+const $volume = $("#volume");
+const $volCtrl = $(".volCtrl");
+const $volNum = $(".volNum");
+const $volBarWrapper = $('.volBarWrapper');
+const $volBar = $('.volBar');
 
 setMusic()
 
@@ -54,7 +60,6 @@ function setMusic() {
   m.audio.src = curMusic.src
   $title.innerText = curMusic.title
   $author.innerText = curMusic.author
-
 }
 
 function secondToText(second) {
@@ -132,4 +137,73 @@ $bar.onmousedown = function (e) {
   $progress.style.width = (percent * 100) + '%';
   m.audio.currentTime = parseInt(m.audio.duration * percent);
   paly()
+}
+// 音量部分
+// 音量控制进度条
+$volBarWrapper.onclick = function (e) {
+  const { offsetHeight: height } = $volBarWrapper
+  const { offsetParent, offsetTop } = $volume
+  const cal = e.screenY - offsetParent.offsetTop - offsetTop + 65
+  const per = ((height - cal) / height) * 100
+  if (per < 1) {
+    $volBar.style.height = '0%';
+    m.audio.volume = 0;
+  } else {
+    $volBar.style.height = per + '%';
+    m.audio.volume = per / 100;
+  }
+  volControl()
+}
+
+// 音量按钮鼠标滑过显示音量控制条,鼠标移出隐藏音量控制条
+$volume.onmouseover = function () {
+  $volCtrl.style.display = "block";
+}
+$volume.onmouseout = function () {
+  $volCtrl.style.display = "none";
+}
+// 鼠标在音量条上移动时候继续显示音量控制条
+$volCtrl.onmousemove = function () {
+  $volCtrl.style.display = "block";
+}
+$volCtrl.onmouseout = function () {
+  $volCtrl.style.display = "none";
+}
+// 监控音量变化，实时显示当前音量
+m.audio.addEventListener('volumechange', function () {
+  $volNum.innerText = m.audio.muted ? "0" : parseInt(m.audio.volume * 100);
+});
+$volume.onclick = function () {
+  if (m.audio.muted) {
+    m.audio.volume = m.cacheVolume
+    $volBar.style.height = (m.audio.volume * 100) + '%';
+    m.audio.muted = false
+  } else {
+    m.cacheVolume = m.audio.volume
+    $volBar.style.height = '0%';
+    m.audio.volume = 0;
+    m.audio.muted = true
+  }
+  volControl()
+}
+function volumeStyle(icon, color) {
+  $volume.className = "iconfont " + icon;
+  $volume.style.color = color;
+}
+// 音量控制函数
+function volControl() {
+  switch (true) {
+    case m.audio.volume === 0:
+      volumeStyle("icon-mute", "#ccc")
+      break;
+    case m.audio.volume > 0 && m.audio.volume <= 0.33:
+      volumeStyle("icon-volume-low", "inherit")
+      break;
+    case m.audio.volume > 0.33 && m.audio.volume <= 0.66:
+      volumeStyle("icon-volume-middle", "inherit")
+      break;
+    case m.audio.volume > 0.66 && m.audio.volume <= 1:
+      volumeStyle("icon-volume-high", "inherit")
+      break;
+  }
 }
